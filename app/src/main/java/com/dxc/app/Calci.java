aa397d54029e0421255ea9fbebe9b544becf25a9
msaigangadha <msaigangadha@IN-5CG0253HFP>
2020-09-21 14:44:20 +0530

    commit

---
+++ app/src/main/java/com/dxc/app/Calci.java
@@ -0,0 +1,39 @@
+package com.dxc.app;
+
+public class Calci {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int result = 	add(4,5);
+
+			System.out.println(result);
+
+	}
+
+
+
+
+
+	public static int add(int  num1,int num2) {
+
+		return num1+num2;
+
+	}
+	
+	
+	public static int sub(int  num1,int num2) {
+
+		return num1-num2;
+
+	}
+	
+	
+	public static int mul(int  num1,int num2) {
+
+		return num1*num2;
+
+	}
+	
+
+}
