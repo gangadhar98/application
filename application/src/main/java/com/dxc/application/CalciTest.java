dcc64b5f457f1fca46fa00d33df3939991a54a50
msaigangadha <msaigangadha@IN-5CG0253HFP>
2020-09-21 14:34:08 +0530

    first commit
    uploading all the files

---
+++ application/src/main/java/com/dxc/application/CalciTest.java
@@ -0,0 +1,47 @@
+package com.dxc.application;
+
+public class CalciTest {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int result = 	add(4,5);
+
+			System.out.println(result);
+
+	}
+
+
+
+
+
+	public static int add(int  num1,int num2) {
+
+		return num1+num2;
+
+	}
+	
+	
+	public static int sub(int  num1,int num2) {
+
+		return num1-num2;
+
+	}
+	
+	
+	public static int mul(int  num1,int num2) {
+
+		return num1*num2;
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
